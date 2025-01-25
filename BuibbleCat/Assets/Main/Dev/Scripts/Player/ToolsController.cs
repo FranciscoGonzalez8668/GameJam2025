7eab2b8c75bd9a6b4e7f490a6156d28ce1352a34
Francisco Gonzalez <francisco_gonzalez2000@hotmail.com>
2025-01-25 20:15:46 -0300

    Se puede prender y apagar el ventilador

--- BuibbleCat/Assets/Main/Dev/Scripts/Player/ToolsController.cs
+++ BuibbleCat/Assets/Main/Dev/Scripts/Player/ToolsController.cs
@@ -6,6 +6,7 @@ public class ToolsController : MonoBehaviour
     [SerializeField] GameObject bubbleGun, fan;
     SoundsSender soundsSender;
     public static System.Action<int> ChangeTool;
+    private bool isFanActive = false;
     private void OnEnable()
     {
         ChangeTool += SwitchTool;
@@ -18,6 +19,14 @@ public class ToolsController : MonoBehaviour
         fan.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleFan();
+        }
+    }
+
     private void SwitchTool(int tool)
     {
         if (tool == 0)
@@ -28,12 +37,21 @@ public class ToolsController : MonoBehaviour
         if (tool == 1)
         {
             PlayFanBtnSound();
+            isFanActive = true;
         }
 
         bubbleGun.SetActive(tool == 0);
         fan.SetActive(tool == 1);
     }
 
+    public void ToggleFan()
+    {
+        isFanActive = !isFanActive;
+        fan.SetActive(isFanActive);
+        PlayFanBtnSound();
+    }
+
     void PlayFanBtnSound() => soundsSender.Play("fanButton");
     void PlayBubbleGunSound() => soundsSender.Play("bubbleGun");
+
 }
