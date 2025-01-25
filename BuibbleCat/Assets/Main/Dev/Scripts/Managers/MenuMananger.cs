18460ea05ff71b60251a240e73f6e3aa9573e6c1
Francisco Gonzalez <francisco_gonzalez2000@hotmail.com>
2025-01-25 17:10:01 -0300

    Se hace scenena de menu con opciones, creditos y play

---
+++ BuibbleCat/Assets/Main/Dev/Scripts/Managers/MenuMananger.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuMananger : MonoBehaviour
+{
+
+    public GameObject optionsPanel;
+    public GameObject creditsPanel;
+
+    public void Start()
+    {
+        if (optionsPanel == null)
+        {
+            Debug.Log("No se asigno el optionsPanel en el inspector");
+        }
+        optionsPanel.SetActive(false);
+
+        if (creditsPanel == null)
+        {
+            Debug.Log("No se asigno el creditsPanl en el inspector");
+        }
+        creditsPanel.SetActive(false);
+    }
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenOptions()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+
+        optionsPanel.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
